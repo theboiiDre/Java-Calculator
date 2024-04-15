bfadf6ec0e033388a43cd28bfa3fe94690bc62a0
theboiiDre <andrefresh0@gmail.com>
2024-04-15 12:15:31 +0100

    updated commit

--- src/main/java/com/example/simplecalculator/SimpleCalculatorProject.java
+++ src/main/java/com/example/simplecalculator/SimpleCalculatorProject.java
@@ -95,9 +95,6 @@ public class SimpleCalculatorProject extends Application {
         btnPercentage.setPrefSize(60,60); // this will set the length to 60 and breadth to 60
         btnPercentage.setFont(fontNm);
 
-
-        //random gffhfgfh
-
         btnPercentage.getStyleClass().add("operator-btn");
 
         btnDivide = new Button("/"); ; // this will display "/" on the button
